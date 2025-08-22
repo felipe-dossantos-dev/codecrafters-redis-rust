15e6230f5e443d54464784cb6656212f8ae3bf77
Felipe Santos <santos.flip@gmail.com>
2025-08-22 10:53:03 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
 #![allow(unused_imports)]
-use std::{io::{BufRead, BufReader, Read, Write}, net::{TcpListener, TcpStream}};
+use tokio::io;
+use tokio::{
+    io::{AsyncBufRead, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
+    loop {
+        let _stream = listener.accept().await;
+        match _stream {
+            Ok((stream, _)) => {
                 println!("accepted new connection");
+                tokio::spawn(async move {
+                    client_process(stream).await;
+                });
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
 
-                let mut buf = [0; 512];
-                loop {
-                    let read_count = _stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
+async fn client_process(mut stream: TcpStream) {
+    let mut buf = [0; 512];
 
-                    _stream.write(b"+PONG\r\n").unwrap();
+    loop {
+        match stream.read(&mut buf).await {
+            Ok(0) => return,
+            Ok(_) => {
+                if stream.write_all(b"+PONG\r\n").await.is_err() {
+                    return;
                 }
             }
             Err(e) => {
                 println!("error: {}", e);
+                return;
             }
         }
     }
